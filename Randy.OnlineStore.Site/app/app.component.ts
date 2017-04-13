6fc4bb9f4fb609ede4000c4c3340326e6e49ff9e
Randy Mujica Diaz <mujicadiazr@gmail.com>
2017-04-13 00:57:45 -0400

    Adding Angular2...

---
+++ Randy.OnlineStore.Site/app/app.component.ts
@@ -0,0 +1,6 @@
+﻿import {Component} from "@angular/core";
+
+@Component({ selector: "my-app", template: "<h1>{{welcomeMessage}}</h1>" })
+export class AppComponent {
+    welcomeMessage: string = "Welcome to Angular 2 ASP.NET MVC 5 project"; 
+}
